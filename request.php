c6a0d369f7b70df8ef6e0085b51f830ee55de12f
Dragoslav Predojevic <dpredojevic@s7designcreative.com>
2018-06-12 14:24:23 +0200

    PR:Added to phpMailer directory and added to their param

--- request.php
+++ request.php
@@ -5,8 +5,9 @@ header('Content-type: application/json; charset=utf-8');
 require_once('includes/database.php');
 define("INFLEX", true);
 
-function sendEmail($mail,$files,$param = FALSE)
+function sendEmail($email,$fullName,$subject)
 {
+    require_once('asset/phpmailer/PHPMailerAutoload.php');
     global $db;
 
     $mail = new PHPMailer();
@@ -18,12 +19,12 @@ function sendEmail($mail,$files,$param = FALSE)
     $mail->Host = ''; // your host
     $mail->Port = 465;
     $mail->IsHTML(true);
-    $mail->Username = ''; //your email
-    $mail->Password = ''; // your pass
-    $mail->SetFrom('gagi.predojevic93@hotmail.com');
-    $mail->FromName = 'Dragoslav Predojevic';
+    $mail->Username = 'gagi.predojevic93@hotmail.com';
+    $mail->Password = '070793';
+    $mail->SetFrom($email);
+    $mail->FromName = $fullName;
     $mail->AddReplyTo('gagi.predojevic93@hotmail.com');
-    $mail->Subject = 'Presenting Repertoair';
+    $mail->Subject = $subject;
     $mail->AddEmbeddedImage('../assets/img/mail/bottom.png', 'logo_2');
     $mail->AddEmbeddedImage('../assets/img/mail/1.png', 'logo_fb');
     $mail->AddEmbeddedImage('../assets/img/mail/2.png', 'logo_gp');
@@ -49,6 +50,74 @@ function sendEmail($mail,$files,$param = FALSE)
     return $mail;
 }
 
+function Login(){
+
+    global $db;
+    $res =  array("status" => "OK");
+
+    if (isset($_POST['email']) && isset($_POST['password']) && !empty($_POST['email']) && !empty($_POST['password'])) {
+
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        $stmt = $db->prepare("SELECT `log_email`,`log_password` FROM `login` WHERE `log_email`= ? AND `log_password`= ?");
+        $stmt->bind_param('ss', $email, $password);
+        $stmt->execute();
+        $stmt->store_result();
+
+
+        if ($stmt->num_rows > 0) {
+            $_SESSION['admin'] = $email;
+            $_SESSION['admin'] = $password;
+        } else {
+            returnError('Invalid username/password');
+        }
+
+    } else {
+        returnError('Errors to database,please contact admin');
+    }
+
+    return $res;
+
+}
+
+function Logout(){
+    $res =  array("status" => "OK");
+    header("location: ../?page=Login");
+    return $res;
+}
+
+function createRepertoair(){
+
+    global $db;
+    $res =  array("status" => "OK");
+
+    if(isset($_POST['title']) && isset($_POST['dateOf']) && isset($_POST['dateTo']) && isset($_POST['description']) && !empty($_POST['title']) && !empty($_POST['dateOf']) && !empty($_POST['dateTo']) && !empty($_POST['description']))
+    {
+        $title = $_POST['title'];
+        $dateOf = $_POST['dateOf'];
+        $dateTo = $_POST['dateTo'];
+        $desc = $_POST['description'];
+        $file = $_POST['file'];
+
+        $stmt = $db->prepare("INSERT INTO `CreateRep`(`title`, `dateOf`, `dateTo`, `description`, `file`) VALUES (?,?,?,?,?)");
+        $stmt->bind_param("ssss", $title, $dateOf,$dateTo,$desc,$file);
+        $stmt->execute();
+        $stmt->store_result();
+
+        if ($stmt->num_rows > 0) {
+            $input = "OK";
+        } else {
+            returnError('Data is not defined');
+        }
+
+    } else {
+        returnError('Network connected');
+    }
+
+    return $res;
+}
+
 
 
 if ( isset( $_POST ) && isset( $_POST[ 'p' ] ) ) {
@@ -58,69 +127,21 @@ if ( isset( $_POST ) && isset( $_POST[ 'p' ] ) ) {
             returnError( "Invalid \'p\' parameter value." );
             break;
         case 'Login':
-            require_once('includes/database.php');
-            $output =  array("status" => "OK");
-
-            if (isset($_POST['email']) && isset($_POST['password']) && !empty($_POST['email']) && !empty($_POST['password'])) {
-
-                $email = $_POST['email'];
-                $password = $_POST['password'];
-
-                $stmt = $db->prepare("SELECT `log_email`,`log_password` FROM `login` WHERE `log_email`= ? AND `log_password`= ?");
-                $stmt->bind_param('ss', $email, $password);
-                $stmt->execute();
-                $stmt->store_result();
-
-
-                if ($stmt->num_rows > 0) {
-                    $_SESSION['admin'] = $email;
-                    $_SESSION['admin'] = $password;
-                } else {
-                    returnError('Invalid username/password');
-                }
-
-            } else {
-                returnError('Errors to database,please contact admin');
-            }
-
-            echo json_encode( $output );
+            $result = Login();
+            echo json_encode( $result );
             break;
 
         case 'Logout':
-                header("location: ../?page=Login");
+            $result = Logout();
             echo json_encode( $result );
             break;
 
         case 'createRep':
-            require_once('includes/database.php');
-            $output =  array("status" => "OK");
-            if(isset($_POST['title']) && isset($_POST['dateOf']) && isset($_POST['dateTo']) && isset($_POST['description']) && !empty($_POST['title']) && !empty($_POST['dateOf']) && !empty($_POST['dateTo']) && !empty($_POST['description']))
-            {
-                $title = $_POST['title'];
-                $dateOf = $_POST['dateOf'];
-                $dateTo = $_POST['dateTo'];
-                $desc = $_POST['description'];
-                $file = $_POST['file'];
-
-                $stmt = $db->prepare("INSERT INTO `CreateRep`(`title`, `dateOf`, `dateTo`, `description`, `file`) VALUES (?,?,?,?,?)");
-                $stmt->bind_param("ssss", $title, $dateOf,$dateTo,$desc,$file);
-                $stmt->execute();
-                $stmt->store_result();
-
-                if ($stmt->num_rows > 0) {
-                    $input = "OK";
-                } else {
-                    returnError('Data is not defined');
-                }
-
-            } else {
-                returnError('Network connected');
-            }
-
-            echo json_encode( $output );
+            $result = createRepertoair();
+            echo json_encode( $result );
             break;
         case 'sendEmail':
-            $result = sendEmail('gagi.predojevic93@hotmail.com', TRUE);
+            $result = sendEmail('gagi.predojevic93@hotmail.com', 'Dragoslav Predojevic','Presenting Repertoair');
             echo json_encode($result);
     }
 }
